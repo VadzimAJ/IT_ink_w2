e8d6db6fc58ca5fd6a3fd249f1bde560df4bfcc6
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-05-22 18:31:11 +0300

    feat: complite hw1_w2

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,30 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number,
+    user: {
+        avatar: any,
+        name: string
+    },
+    message: {
+        text: string,
+        time: string
+    }
+}
+
+export type FriendMessageType = {
+    id: number,
+    user: {
+        avatar: any,
+        name: string
+    },
+    message: {
+        text: string,
+        time: string
+    }
+}
+
 
 // структуру объекта не менять
 export const message0: MessageType = {
--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
+import { MessageType } from '../HW1'
+
+type MessagePropsType = {
+    message: MessageType // Используйте тип MessageType
+}
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+const FriendMessage = (props: MessagePropsType) => {
     return (
         <div
             id={'hw1-friend-message-' + props.message.id}
@@ -12,16 +17,16 @@ const FriendMessage = (props: any) => {
                 <img
                     id={'hw1-friend-avatar-' + props.message.id}
                     // создаёт студент
-
+                    src={props.message.user.avatar}
                     //
                 />
                 <div className={s.friendText}>
                     <div
                         id={'hw1-friend-name-' + props.message.id}
                         className={s.friendName}
-                    >
+                    >   
                         {/*создаёт студент*/}
-
+                        
                         {/**/}
                     </div>
                     <pre
@@ -29,7 +34,7 @@ const FriendMessage = (props: any) => {
                         className={s.friendMessageText}
                     >
                         {/*создаёт студент*/}
-
+                        {props.message.message.text}
                         {/**/}
                     </pre>
                 </div>
@@ -39,7 +44,7 @@ const FriendMessage = (props: any) => {
                 className={s.friendTime}
             >
                 {/*создаёт студент*/}
-
+                {props.message.message.time} 
                 {/**/}
             </div>
         </div>
