e88ff930389d4fc5020093741561b4f473f76b1b
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-06-17 20:16:42 +0300

    feat: complite HM 2

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,40 +4,52 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: Array<AffairType> // need to fix any
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
+
+    const {data,
+            setFilter, 
+            deleteAffairCallback, 
+            filter} = props
+
     const setAll = () => {
+        setFilter('all')
         // need to fix
     }
     const setHigh = () => {
+        setFilter('high')
         // need to fix
     }
     const setMiddle = () => {
+        setFilter('middle')
         // need to fix
     }
     const setLow = () => {
+        setFilter('low')
         // need to fix
     }
 
-    const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
-    const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ? ' ' + s.active : '')
-    const cnMiddle = s.button + ' ' + s.middle + (props.filter === 'middle' ? ' ' + s.active : '')
-    const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ? ' ' + s.active : '')
+    const cnAll = s.button + ' ' + s.all + (filter === 'all' ? ' ' + s.active : '')
+    const cnHigh = s.button + ' ' + s.high + (filter === 'high' ? ' ' + s.active : '')
+    const cnMiddle = s.button + ' ' + s.middle + (filter === 'middle' ? ' ' + s.active : '')
+    const cnLow = s.button + ' ' + s.low + (filter === 'low' ? ' ' + s.active : '')
 
     const mappedAffairs = props.data.map((a: AffairType) => (
         <Affair
             key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
             affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
+            deleteAffairCallback={deleteAffairCallback}
         />
     ))
 
     return (
+
+        
         <div>
             <div className={s.buttonContainer}>
                 <button
